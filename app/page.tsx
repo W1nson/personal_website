40ecdffeb69b786487675aed709eda8690b8547e
Winson <wchen157@ucsc.edu>
2023-11-27 01:00:16 -0800

    solve the background

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,18 @@ export default function Page() {
 	
 
 
-
 	return (
-		<h1>Home</h1>
+		
+		<div className="grid grid-rows-3 col-span-full"> 
+
+			<div className="items-center justify-center h-[150px] w-[100%] bg-sky-500">
+				<h1 className="justify-self-center">Welcome to Winson's Website</h1>
+			</div>
+			
+
+
+			<h1>News</h1>
+		</div>
+		
 	);
 }
--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -6,7 +6,7 @@ export default async function SideBar() {
 
 
 	return(
-		<div className='flex flex-col w-[200px] h-screen bg-white items-center'>
+		<div className='flex flex-col w- h-screen bg-white items-center'>
 			
 			<Image src='/profile.png' className='p-1 rounded-full' height={150} width={150} alt='Profile Picture'/>
 			<strong className="text-black text-xl">Winson Chen</strong>

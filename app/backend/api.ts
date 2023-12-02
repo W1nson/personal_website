6b3723fd59273a229dd487e699ecba64586c64bd
Winson Chen <winsonchen108@gmail.com>
2023-12-01 19:50:38 -0800

    build fix

--- app/backend/api.ts
+++ app/backend/api.ts
@@ -5,8 +5,19 @@ import { sql } from '@vercel/postgres';
 
 
 
-
-
 export async function createUser() { 
-	
+
+	try {
+		await sql`
+		INSERT INTO invoices (customer_id, amount, status, date)
+		VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+	} catch (error) { 
+		return {
+			message: 'Database Error: Failed to Create Invoice.' 
+		}
+	}
 }
+
+
+
+
